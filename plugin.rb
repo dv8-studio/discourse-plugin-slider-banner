33518014a8266399d94f022495e672038583be8e
purple1007 <purple1007@gmail.com>
2020-02-01 23:49:29 +0800

    update style

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,8 @@
 # name: slider-banner
 # about: 廣告幻燈片
 # version: 0.0.1
-# authors: kenwheeler
+# authors: this plugin base form kenwheeler
+# url: https://github.com/purple1007/discourse-plugin-slider-banner
 
 register_asset "stylesheets/slick-custom.scss"
 register_asset "javascripts/plugin/slick.js"
