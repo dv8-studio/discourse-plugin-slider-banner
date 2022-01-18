adf92d9224d11e6102bc0af80365ad2d2428c445
DevKurka <42714242+DevKurka@users.noreply.github.com>
2022-01-18 17:37:48 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: slider-banner
-# about: 廣告幻燈片
-# version: 0.0.1
-# authors: this plugin base form kenwheeler
-# url: https://github.com/purple1007/discourse-plugin-slider-banner
+# about: Discourse slider banner
+# version: 0.0.2
+# authors: purple1007, DevKurka
+# url: https://github.com/dv8-studio/discourse-plugin-slider-banner
 
 register_asset "stylesheets/slick-custom.scss"
 register_asset "javascripts/plugin/slick.js"
