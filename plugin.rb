6ee07437d1771c631ee75ccf2da02a4563738c31
Etheroit <42714242+Etheroit@users.noreply.github.com>
2024-02-16 17:02:04 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,10 @@
 # name: slider-banner
 # about: Discourse slider banner
-# version: 0.0.2
-# authors: purple1007, DevKurka
+# version: 0.0.3
+# authors: purple1007, Etheroit
 # url: https://github.com/dv8-studio/discourse-plugin-slider-banner
 
 register_asset "stylesheets/slick-custom.scss"
-register_asset "javascripts/plugin/slick.js"
 
 
 enabled_site_setting :slider_banner_enabled
